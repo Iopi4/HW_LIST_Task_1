a6c4fb9cc822ef25553fe85f38c036cf49ccdaa0
Iopi4 <relentler@gmail.com>
2026-07-10 13:19:11 +0500

    Task_1. Согласно заданию. + Добавлены отработка некорректного ввода данных пользователем.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+/**
+ * В задании 1 (обязательное) введено улучшение для безопасности ввода данных.
+ * Добавлены отработка некорректного ввода данных пользователем.
+ */
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -14,13 +19,23 @@ public class Main {
             System.out.println("2. Показать дела");
             System.out.println("3. Удалить дело по номеру");
             System.out.println("4. Удалить дело по названию");
-            System.out.print("Ваш выбор: ");
 
-            String input = scanner.nextLine();
-            int operation  = Integer.parseInt(input);
+            boolean correctInput = false; //переменная для определения корректности ввода
+            int operation = -1;
 
+            //Добавим отдельный цикл для проверки корректности ввода
+            while (!correctInput){
+                System.out.print("Ваш выбор: ");
+                String input = scanner.nextLine();
+                try {
+                    operation = Integer.parseInt(input);
+                    correctInput = true;
+                } catch (NumberFormatException e){
+                    System.out.println("Ошибка. Введите корректный номер операции");
+                }
+            }
 
-            if (operation == 0){
+            if (operation == 0) {
                 break;
             }
 
@@ -29,11 +44,16 @@ public class Main {
                     System.out.println();
                     System.out.print("Введите название задачи: ");
                     String task = scanner.nextLine();
-                    todoList.add(task);
-                    System.out.println("Добавлено!");
+                    //проверим на совпадение дела
+                    if (todoList.contains(task)){
+                        System.out.println("Уже существует!");
+                    } else {
+                        todoList.add(task);
+                        System.out.println("Добавлено!");
+                    }
                     todoListsPrint(todoList);
-
                     break;
+
                 case 2:
                     todoListsPrint(todoList);
                     break;
@@ -42,9 +62,20 @@ public class Main {
                     System.out.println();
                     System.out.print("Введите номер для удаления: ");
                     String number = scanner.nextLine();
-                    int delete  = Integer.parseInt(number);
-                    todoList.remove(delete - 1);
-                    System.out.println("Удалено!");
+                    int delete = -1; //переменная для присвоения номера дела
+                    //проверим на совпадение номера дела
+                    try {
+                        delete  = Integer.parseInt(number);
+                        if (delete < 1 || delete > todoList.size()){
+                            System.out.println("Ошибка. Такого номера нет");
+                        } else {
+                            todoList.remove(delete - 1);
+                            System.out.println("Удалено!");
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("Ошибка. Надо ввести число");
+                    };
+
                     todoListsPrint(todoList);
                     break;
 
@@ -52,15 +83,19 @@ public class Main {
                     System.out.println();
                     System.out.print("Введите название задачи для удаления: ");
                     String name = scanner.nextLine();
-                    todoList.remove(name);
-                    System.out.println("Удалено!");
+                    //Создадим переменную booleen для отслеживания удалено дело или нет
+                    boolean removed =  todoList.remove(name);
+                    if (removed){
+                        System.out.println("Удалено!");
+                    } else {
+                        System.out.println("Ошибка. Такой задачи нет");
+                    }
                     todoListsPrint(todoList);
                     break;
 
                 default:
                     System.out.println("Такой операции нет");
             }
-
         }
         System.out.println();
     }
